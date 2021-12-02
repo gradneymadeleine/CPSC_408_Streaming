8b3924628dc4c60c67c8ba27d10c1a24ab07a631
Kaitlynn <klkyamauchi@gmail.com>
2021-12-01 22:09:27 -0800

    Added create_tables.sql

---
+++ create_tables.sql
@@ -0,0 +1,51 @@
+CREATE TABLE netflix(
+show_id VARCHAR(10) NOT NULL PRIMARY KEY,
+type ENUM('Movie', 'TV Show'),
+title VARCHAR(50),
+director VARCHAR(30),
+cast VARCHAR(100),
+country VARCHAR(20),
+date_added DATE,
+release_year YEAR,
+rating VARCHAR(10),
+duration VARCHAR(20)
+);
+
+CREATE TABLE hulu(
+show_id VARCHAR(10) NOT NULL PRIMARY KEY,
+type ENUM('Movie', 'TV Show'),
+title VARCHAR(50),
+director VARCHAR(30),
+cast VARCHAR(100),
+country VARCHAR(20),
+date_added DATE,
+release_year YEAR,
+rating VARCHAR(10),
+duration VARCHAR(20)
+);
+
+CREATE TABLE disney(
+show_id VARCHAR(10) NOT NULL PRIMARY KEY,
+type ENUM('Movie', 'TV Show'),
+title VARCHAR(50),
+director VARCHAR(30),
+cast VARCHAR(100),
+country VARCHAR(20),
+date_added DATE,
+release_year YEAR,
+rating VARCHAR(10),
+duration VARCHAR(20)
+);
+
+CREATE TABLE amazon(
+show_id VARCHAR(10) NOT NULL PRIMARY KEY,
+type ENUM('Movie', 'TV Show'),
+title VARCHAR(50),
+director VARCHAR(30),
+cast VARCHAR(100),
+country VARCHAR(20),
+date_added DATE,
+release_year YEAR,
+rating VARCHAR(10),
+duration VARCHAR(20)
+);
