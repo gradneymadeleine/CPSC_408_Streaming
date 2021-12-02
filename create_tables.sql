046001c2a7629d10d77a983b609c9861c524d88e
Kaitlynn <klkyamauchi@gmail.com>
2021-12-02 01:10:53 -0800

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -1,16 +1,16 @@
 CREATE TABLE netflix(
 show_id VARCHAR(10) NOT NULL PRIMARY KEY,
-type VARCHAR(10),
+show_movie VARCHAR(10),
 title VARCHAR(50),
 director VARCHAR(30),
-cast VARCHAR(1000),
+actors VARCHAR(1000),
 country VARCHAR(20),
 date_added DATE,
 release_year YEAR,
 rating VARCHAR(10),
 duration VARCHAR(20),
 listed_in VARCHAR(20),
-description VARCHAR(1000)
+summary VARCHAR(1000)
 );
 
 CREATE TABLE hulu(
